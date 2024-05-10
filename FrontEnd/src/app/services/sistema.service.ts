2a43607390588b02cb39adf85d675216e4554ff3
Delmasquio <114113443+Delmasquio@users.noreply.github.com>
2024-05-10 18:28:08 -0300

    Update sistema.service.ts

--- FrontEnd/src/app/services/sistema.service.ts
+++ FrontEnd/src/app/services/sistema.service.ts
@@ -9,38 +9,39 @@ import { SistemaFinanceiroDTO } from '../models/SistemaFinanceiroDTO';
 })
 
 export class SistemaService {
+    private apiUrl = 'https://localhost:7154/api/sistema-financeiro';
 
-    constructor(private httpClient: HttpClient) {
-    }
+    constructor(private http: HttpClient) {}
 
     private readonly baseURL = environment["endPoint"];
 
     AdicionarSistemaFinanceiro(sistemaFinanceiro: SistemaFinanceiroDTO) {
-        console.log("teste");
-        console.log(sistemaFinanceiro);
-        console.log(this.httpClient.post<SistemaFinanceiroDTO>(`${this.baseURL}/sistema-financeiro`,sistemaFinanceiro))
-        return this.httpClient.post<SistemaFinanceiroDTO>(`${this.baseURL}/sistema-financeiro`,sistemaFinanceiro)
+        return this.http.post<SistemaFinanceiroDTO>(`${this.baseURL}/sistema-financeiro`,sistemaFinanceiro)
+    }
+   
+    ListaSistemasUsuario(emailUsuario: string) {
+        return this.http.get(`${this.baseURL}/sistema-financeiro`);
     }
 
-    ListaSistemasUsuario(emailUsuario: string) {
-        return this.httpClient.get(`${this.baseURL}/sistema-financeiro`);
+    DeletarSistemaFinanceiro(id:any, nome:any, mes:any, ano:any, diaFechamento:any) {
+        return this.http.delete<any>(`${this.baseURL}/sistema-financeiro/${id}`).toPromise();
     }
 
     CadastrarUsuarioNoSistema(idSistema: number, emailUsuario: string) {
-        return this.httpClient.post<any>(`${this.baseURL}/usuario-sistema-financeiro/cadastrar-usuario-sistema?idSistema=${idSistema}&emailUsuario=${emailUsuario}`, null)
+        return this.http.post<any>(`${this.baseURL}/usuario-sistema-financeiro/cadastrar-usuario-sistema?idSistema=${idSistema}&emailUsuario=${emailUsuario}`, null)
     }
 
     ExecuteCopiaDespesasSistemafinanceiro() {
-        return this.httpClient.post<any>(`${this.baseURL}/ExecuteCopiaDespesasSistemafinanceiro`, null)
+        return this.http.post<any>(`${this.baseURL}/ExecuteCopiaDespesasSistemafinanceiro`, null)
     }
 
 
     ObterSistemaFinanceiro(id: number) {
-        return this.httpClient.get(`${this.baseURL}/sistema-financeiro/obter-sistema-financeiro?id=${id}`);
+        return this.http.get(`${this.baseURL}/sistema-financeiro/obter-sistema-financeiro?id=${id}`);
     }
 
     AtualizarSistemaFinanceiro(sistemaFinanceiro: SistemaFinanceiroDTO) {
-        return this.httpClient.put<SistemaFinanceiroDTO>(`${this.baseURL}/sistema-financeiro/atualizar-sistema-financeiro`,
+        return this.http.put<SistemaFinanceiroDTO>(`${this.baseURL}/sistema-financeiro/atualizar-sistema-financeiro`,
             sistemaFinanceiro)
     }
 
