5ded28d5d0c994878947b34b1dfe9255e2075947
NicolasTeixeiraa <103547208+NicolasTeixeiraa@users.noreply.github.com>
2024-05-11 17:02:03 -0300

    Acerto na API - CORS

--- Puc_Sistema_Financeiro/Puc_Sistema_Financeiro/Program.cs
+++ Puc_Sistema_Financeiro/Puc_Sistema_Financeiro/Program.cs
@@ -1,6 +1,7 @@
 using Puc_Sistema_Financeiro.Models;
 using Puc_Sistema_Financeiro.Services;
 using WebApiMongoDB.Services;
+using Microsoft.AspNetCore.Cors;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -24,7 +25,21 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:4200") // Substitua pela URL do seu aplicativo Angular
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
+
+app.UseRouting();
+app.UseCors("AllowAngularApp");
 
 if (app.Environment.IsDevelopment())
 {
