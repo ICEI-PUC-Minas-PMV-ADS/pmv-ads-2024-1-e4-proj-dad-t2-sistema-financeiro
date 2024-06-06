bce2b705cf8d205a96f3ee32b83f0060ae8a504c
NicolasTeixeiraa <103547208+NicolasTeixeiraa@users.noreply.github.com>
2024-06-05 21:39:25 -0300

    Delete TelaDespesaPuc directory

--- TelaDespesaPuc/src/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment =
-{
-    production: false,
-     endPoint: 'https://localhost:7125/api'
-    //endPoint:  'http://localhost:5093/api'
-    //endPoint:'http://192.168.0.199:9021/api'
-}
